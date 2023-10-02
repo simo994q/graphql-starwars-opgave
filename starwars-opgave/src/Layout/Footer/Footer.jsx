c0549ed29f7f494714a30530008100de4533190c
simo994q <simonkn1@hotmail.com>
2023-10-02 12:08:30 +0200

    some styling

--- starwars-opgave/src/Layout/Footer/Footer.jsx
+++ starwars-opgave/src/Layout/Footer/Footer.jsx
@@ -1,9 +1,11 @@
-
+import style from './Footer.module.scss'
 
 export const Footer = () => {
     return (
         <>
-        footer
+            <div className={style.footerContainer}>
+                footer
+            </div>
         </>
     )
 }
