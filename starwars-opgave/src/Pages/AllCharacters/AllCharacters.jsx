b7099dd6e823a19a575e796ad1f7902d7f029b98
simo994q <simonkn1@hotmail.com>
2023-10-04 11:25:13 +0200

    search function wow

--- starwars-opgave/src/Pages/AllCharacters/AllCharacters.jsx
+++ starwars-opgave/src/Pages/AllCharacters/AllCharacters.jsx
@@ -11,6 +11,9 @@ export const AllCharacters = () => {
 
     const [singleCharacterData, setSingleCharacterData] = useState()
 
+    const [inputText, setInputText] = useState('')
+    const [searchData, setSearchData] = useState()
+
     Modal.setAppElement('#root');
 
     const customStyles = {
@@ -31,32 +34,48 @@ export const AllCharacters = () => {
 
     console.log(data);
 
-    if (isLoading) return <p>Loading...</p>
+    const search = () => {
+        let clone = data.allPeople.people.map((item) => item)
+        let result = clone.filter((item) => item.name.toLowerCase().includes(inputText.toLowerCase()))
+        console.log(result);
+        setSearchData(result)
+    }
 
-    if (error) return <p>Error: {error.message}</p>
 
-    return (
-        <>
-            <div className={style.charactersContainer}>
-                {data.allPeople.people.map((item, index) => {
-                    return (
-                        <h2 key={index} onClick={() => { setSingleCharacterData(item), setIsOpen(true) }}>{item.name}</h2>
-                    )
-                })}
+if (isLoading) return <p>Loading...</p>
+
+if (error) return <p>Error: {error.message}</p>
+
+return (
+    <>
+        <div>
+            <input type="text" onChange={(e) => setInputText(e.target.value)} placeholder="Search" style={{ fontSize: '24px', width: '300px', color: 'black' }} />
+            <button onClick={() => search()} style={{ fontSize: '24px', width: '300px', backgroundColor: 'black' }}>Search</button>
+        </div>
+        <div className={style.charactersContainer}>
+            {!searchData ? data.allPeople.people.map((item, index) => {
+                return (
+                    <h2 key={index} onClick={() => { setSingleCharacterData(item), setIsOpen(true) }}>{item.name}</h2>
+                )
+            }) : searchData.map((item, index) => {
+                return (
+                    <h2 key={index} onClick={() => { setSingleCharacterData(item), setIsOpen(true) }}>{item.name}</h2>
+                )
+            })}
+        </div>
+        <Modal
+            isOpen={modalIsOpen}
+            style={customStyles}
+            className={style.modal}
+            overlayClassName={style.modalOverlay}>
+            <div className={style.modalContentContainer}>
+                <h2>{singleCharacterData?.name}</h2>
+                <p>Birth year: {singleCharacterData?.birthYear}</p>
+                <p>Species: {singleCharacterData?.species ? singleCharacterData?.species.name : 'Human'}</p>
+                <p>Homeworld: {singleCharacterData?.homeworld.name}</p>
             </div>
-            <Modal
-                isOpen={modalIsOpen}
-                style={customStyles}
-                className={style.modal}
-                overlayClassName={style.modalOverlay}>
-                <div className={style.modalContentContainer}>
-                    <h2>{singleCharacterData?.name}</h2>
-                    <p>Birth year: {singleCharacterData?.birthYear}</p>
-                    <p>Species: {singleCharacterData?.species ? singleCharacterData?.species.name : 'Human'}</p>
-                    <p>Homeworld: {singleCharacterData?.homeworld.name}</p>
-                </div>
-                <button onClick={() => setIsOpen(false)}>Close</button>
-            </Modal>
-        </>
-    )
+            <button onClick={() => setIsOpen(false)}>Close</button>
+        </Modal>
+    </>
+)
 }
